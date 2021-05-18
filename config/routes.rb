df3b15626687fe191fdb66ac99d1f2ad04df1e7e
Douglas Berkley <douglasmberkley@gmail.com>
2021-05-18 11:10:01 +0900

    all 7 CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # HTTP_VERB '/path', to: 'controller#action', as: :prefix
+  #CRUD
+
+  # Create
+  # # we need a page for a form
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # # we need to send the form data somewhere
+  # post '/restaurants', to: 'restaurants#create', as: :restaurants
+
+  # # Read
+  # get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+
+  # # Edit
+  # # we need a page for a form
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # # we need to send the form data somewhere
+  # patch '/restaurants/:id', to: 'restaurants#update'
+
+  # # Destroy
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  # resources :restaurants
 end
